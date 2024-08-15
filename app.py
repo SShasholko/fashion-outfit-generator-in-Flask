2fbbb7300ace3fb0e85db013f218e9de086e2b04
Svitlana Shasholko <shasholko.sveta@gmail.com>
2024-08-15 12:58:43 +0100

    About page

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
